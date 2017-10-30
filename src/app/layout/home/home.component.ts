c9d51da660fd5a7b80299904b46aa51949ba36d1
alpitg <alpitgajbhiye@gmail.com>
2017-10-30 17:47:19 +0530

    Second Commit

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -13,19 +13,7 @@ import {
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [
-    trigger('popOverState', [
-      state('show', style({
-        opacity: 1
-      })),
-      state('hide', style({
-        opacity: 0
-      })),
-      transition('show => hide', animate('600ms ease-out')),
-      transition('hide => show', animate('1000ms ease-in'))
-    ]),
-
-  ]
+  
 })
 export class HomeComponent implements OnInit {
 
@@ -38,12 +26,6 @@ export class HomeComponent implements OnInit {
   }
 
 
-  get stateName() {
-    return this.show ? 'show' : 'hide'
-  }
-  toggle() {
-    this.show = !this.show;
-  }
 
 
 }
